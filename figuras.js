d9cadb130f2ffbe72cc73ec8ee0f926ed414495e
Carlos Jesus Perez Dugarte <carlos.perez@vicentelopez.gov.ar>
2022-01-31 12:52:42 -0300

    Se comenzó la constrcuccion del triangulo

--- figuras.js
+++ figuras.js
@@ -1,3 +1,5 @@
+//Lado del cuadrado:
+
 const ladoCuadrado = 5;
 
 console.log('Los lados del cuadrado miden: ' + ladoCuadrado);
@@ -9,3 +11,12 @@ console.log('El perímetro cuadrado es: ' + perimetroCuadrado);
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 
 console.log('El area del cuadrado es de: ' + areaCuadrado);
+
+//Lado del triángulo:
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+
+console.log('Los lados del triángulo miden: ' + ladoTriangulo1)
