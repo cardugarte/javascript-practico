492c3bb77749acdb0a4b86c176f09dc146d98ed1
Carlos Jesus Perez Dugarte <carlos.perez@vicentelopez.gov.ar>
2022-01-31 10:49:43 -0300

    Se crean las variables del cuadrado

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# javascript-practico
+# Curso práctico de Javascript
+
+## Taller #1: figuras geométricas:
+
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JavaScript en HTML
+
+
+
+
+
---
+++ figuras.js
@@ -0,0 +1,11 @@
+const ladoCuadrado = 5;
+
+console.log('Los lados del cuadrado miden: ' + ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log('El perímetro cuadrado es: ' + perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log('El area del cuadrado es de: ' + areaCuadrado);
